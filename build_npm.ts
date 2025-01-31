78e1c545d8a37e06b3e131b2b2efb613b178c66c
Antogamer <antoniocavaliere2009@gmail.com>
2025-01-31 22:43:30 +0100

    bruh

--- build_npm.ts
+++ build_npm.ts
@@ -1,8 +1,8 @@
 import { build, emptyDir } from "https://deno.land/x/dnt@0.33.1/mod.ts";
-import pkg from "./package.json" assert { type: "json" };
+import pkg from "./package.json" with { type: "json" };
 
 const tag = Deno.env.get("GITHUB_REF");
-const version = "0.1.0" // i don't even understand how this worked on @telegraf/entity but whatever
+const version = "0.1.1" // i don't even understand how this worked on @telegraf/entity but whatever
 
 if (!version)
 	throw new Error("VERSION environment variable not found. Use similar to `VERSION=0.1.0 deno run -A build_npm.ts`");
